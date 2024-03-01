2e6cd19556d378301ad07d129846f69333c6b48e
Bhupesh Malhotra <bhupeshmalhotra@gmail.com>
2024-03-01 10:22:51 -0500

    updt webpack config and dev pkgs for NodeJS > v16

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const config = {
   entry: './src/visual.ts',
   output: {
     filename: 'visual.js',
     path: path.join(__dirname, 'dist'),
-    library: 'WynVisualClass',
-    libraryTarget: 'umd',
+    library: {
+      name: 'WynVisualClass',
+      type: 'umd',
+    },
     libraryExport: 'default'
   },
   plugins: [
@@ -18,35 +20,28 @@ const config = {
     }),
   ],
   optimization: {
-    minimizer: [new OptimizeCSSAssetsPlugin({}), new UglifyJsPlugin()],
+    minimizer: [new CssMinimizerPlugin({}), new TerserPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.(ts)$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'awesome-typescript-loader?silent=true'],
+        use: ['babel-loader', 'ts-loader'],
       },
       {
-        test: /\.less$/,
+        test: /\.less|.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
       },
       {
         test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader'
-          },
-        ],
+        type: 'asset/inline',
       },
     ],
     unknownContextCritical: false,
   },
-  devServer: {
-    host: '0.0.0.0',
-    index: 'index.html',
-    port: 8081,
-    publicPath: '/assets/'
+  resolve: {
+    extensions: ['.ts', '.js', '.json', '.css', '.less'],
   },
   mode: 'development',
 };
